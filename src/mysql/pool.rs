bc5e7e205a8bc8d4c09b4e3589dd45482c4062d3
manito <wwwbjqcom@127.com>
2020-06-29 16:40:24 +0800

    1

--- src/mysql/pool.rs
+++ src/mysql/pool.rs
@@ -510,7 +510,7 @@ impl MysqlConnectionInfo{
         packet.push(0);
         packet.push(0x0e);
         self.conn.set_write_timeout(Some(Duration::new(0,1000)))?;
-        if let Err(e) = self.conn.write(&packet){
+        if let Err(e) = self.conn.try_write(&packet){
             debug(e.to_string());
             return Ok(false);
         };
