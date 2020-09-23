f55d9d6e10edfed216989e6ac65953bd9ec0e3b3
wwwbjqcom <wwwbjqcom@126.com>
2020-09-23 14:21:49 +0800

    bug

--- src/dbengine/client.rs
+++ src/dbengine/client.rs
@@ -91,7 +91,8 @@ impl ClientResponse {
                 if let Err(e) = self.parse_query_packet(handler).await{
                     error!("{}",&e.to_string());
                     self.send_error_packet(handler, &e.to_string()).await?;
-                    handler.per_conn_info.reset_connection().await?;
+                    handler.per_conn_info.return_connection(0).await?;
+                    //handler.per_conn_info.reset_connection().await?;
                     //return Err(Box::new(MyError(e.to_string().into())));
                 };
             }
