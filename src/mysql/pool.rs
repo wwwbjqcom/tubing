7490aab38a43bdabf3d73a9535e0fa517b07ff74
wwwbjqcom <wwwbjqcom@126.com>
2020-12-09 17:23:31 +0800

    1

--- src/mysql/pool.rs
+++ src/mysql/pool.rs
@@ -1404,7 +1404,7 @@ impl MysqlConnectionInfo{
 
     /// send packet and return response packet for sync
     fn __send_packet(&mut self, packet: &Vec<u8>) -> Result<(Vec<u8>, PacketHeader)> {
-        self.conn.take_error()?;
+        let _ = self.conn.try_clone()?;
         debug!("write packet to mysql connection (timeout {:?})", self.conn.write_timeout().unwrap());
         self.conn.write_all(packet)?;
         debug("write packet to socket(db): OK");
