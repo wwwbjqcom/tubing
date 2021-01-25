391b4b32688e8662c12fff06deb67cec8c1d09e0
wwwbjqcom <wwwbjqcom@126.com>
2021-01-25 16:19:25 +0800

    1

--- src/dbengine/client.rs
+++ src/dbengine/client.rs
@@ -92,10 +92,14 @@ impl ClientResponse {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         if cur_timestamp - self.cur_timestamp >= 1000 {
 =======
         if cur_timestamp - self.cur_timestamp >= 100 {
 >>>>>>> parent of aeebf51... test
+=======
+        if cur_timestamp - self.cur_timestamp >= 10000 {
+>>>>>>> parent of 7992492... 1
 =======
         if cur_timestamp - self.cur_timestamp >= 10000 {
 >>>>>>> parent of 7992492... 1
